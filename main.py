56f94d850dfffcc5a376f5398f5c13d7f439d97d
lollo03 <03lollo@gmail.com>
2021-01-13 21:41:38 +0100

    Sistemato cron, modifice html
    Ora cron aggiorna tutto alle 00:00 e la pagina mostra data e ora di aggiornamento.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import io
 import pandas as pd
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 regioni = ["Abruzzo", "Basilicata", "Calabria", "Campania", "Emilia-Romagna", "Friuli Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "P.A. Bolzano", "P.A. Trento", "Piemonte", "Puglia", "Sardegna", "Sicilia", "Toscana", "Umbria", "Valle d'Aosta", "Veneto"]
 abitanti = [1293941, 553254, 1894110, 5712143, 4464119, 1206216, 5755700, 1524826, 10027602, 1512672, 300516, 520891, 538223 ,4311217, 3953305, 4875290,4875290 ,3692555,870165,125034,4879133]
@@ -43,5 +43,6 @@ with open("template.html", "r+") as f:
                     if regione.find(x) != -1:
                         line = f"{dati[j]},"
                     j+=1
-                    
+            if "<!-- data_ora -->" in line:
+                line = f"{datetime.now().strftime('%d/%m/%Y alle %H:%M')}"
             wf.write("\n" + line)
